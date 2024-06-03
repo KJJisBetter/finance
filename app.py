df3507b0a5acb1e85abb20add7540e43b82a17cd
Kevin Javier <kevinelfuelte@gmail.com>
2024-06-02 20:57:57 -0400

    chore: Update Flask and cs50 imports in app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 
-from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from cs50 import SQL # type: ignore
+from flask import Flask, flash, redirect, render_template, request, session # type: ignore
 from flask_session import Session
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash # type: ignore
 
 from helpers import apology, login_required, lookup, usd
 
